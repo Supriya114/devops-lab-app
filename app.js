52b6c5ba0e521814f484e9f37fd780baec9732fd
supriya <supriyabharath114@gmail.com>
2026-05-13 16:07:05 +0000

    Testing webnook automation

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("CI/CD Pipeline Working Successfully!");
+    res.send("Webhook Trigger Working Successfully!");
 });
 
 app.listen(3000, () => {
